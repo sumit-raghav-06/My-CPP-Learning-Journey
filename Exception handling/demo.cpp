b6fe575266f35177417168e18b96f26a6d45854b
sumit-raghav-06 <sumitraghavxc@gmail.com>
2025-11-19 12:22:10 +0530

    Added Exception handling

---
+++ Exception handling/demo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a=5;
+ int b=0;
+ try{
+    if(b==0){
+        throw 1;
+        c=a/b;
+        cout<<c<<endl;
+    
+    catch(int e){
+        cout<<"divison by zero "<<e<<endl;
+    }
+    cout<<"bye<"<<endl;
+
+ }
